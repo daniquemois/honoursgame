b6348ee4f06164f84bc98843e1e45ab9d97ef37a
Danique Lammertink <danique.lammertink@hva.nl>
2022-04-25 11:13:46 +0200

    Working on the background

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express')
-const port = 3000
+const port = 1337;
+const path = require('path');
 const app = express();
 
+const routes = require("./routes");
 
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+app.use(bodyParser.json());
+
+const { engine } = require ('express-handlebars');
+app.engine('hbs', engine({ 
+ 	extname: 'hbs', 
+ 	defaultLayout: 'main', 
+ 	layoutsDir: __dirname + '/views/layouts/',
+ 	partialsDir: __dirname + '/views/partials/',
+	helpers: require('./config/hbs-helpers')
+}));
+app.set('view engine', 'hbs');
+app.set("views", "./views");
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.use('/', urlencodedParser, routes);
 app.listen(port);
